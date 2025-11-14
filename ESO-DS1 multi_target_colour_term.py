b9b3ac5c6be13681659d3b99def4b72454fd8a3b
Luke Scott <lukescottnova@gmail.com>
2025-11-14 19:19:09 +0000

    Updated target names list

--- ESO-DS1 multi_target_colour_term.py
+++ ESO-DS1 multi_target_colour_term.py
@@ -14,7 +14,7 @@ all_fits_path = pathlib.Path(root_dir/"Data_set_1"/"block_1"/"ALL_FITS")
 
 pix_mask=CT.load_bad_pixel_mask(calib_path)
 
-tgt_names=["P2004F3","149P","94P","93P","74P","2009AU16","P2005R2","P29"]
+tgt_names=["P2004F3","94P","93P","74P","2009AU16","P2005R2","P29","50P","P113","149P"]
 
 filter="R#642"
 cat_filter="rmag"
