88288b811aec02b7b8077b84eb02505c4407b870
Luke Scott <lukescottnova@gmail.com>
2025-11-14 01:10:26 +0000

    Added clean_unsolved function to scan for and remove un-plate-solved images from the processing folder

--- utility_functions.py
+++ utility_functions.py
@@ -2,7 +2,9 @@ import data_organisation_tools as DOT
 import calibration_tools as CT
 
 from ccdproc import ImageFileCollection
+import ccdproc as ccdp
 import pathlib
+import os
 import numpy as np
 
 root_dir = pathlib.Path(__file__).resolve().parent
@@ -15,4 +17,19 @@ def report_names(all_fits_path):
     all_objects=np.array(lights.summary["object"])
     print(np.unique(all_objects))
 
-report_names(all_fits_path)
+def clean_unsolved(calib_path):
+    lights=ImageFileCollection(calib_path,keywords='*')
+    criteria = {"plate_solved":False}
+    file_names=lights.files_filtered(**criteria)
+    print("FILES TO BE DELETED: ")
+    for name in file_names:
+        print(name)
+    print("-"*10)
+    confirm=input(("DELETE "+str(len(file_names))+" FILES???"))
+    confirm=input(("ARE YOU ABSOLUTELY SURE?"))
+    for name in file_names:
+        os.remove(calib_path/name)
+
+
+#report_names(all_fits_path)
+clean_unsolved(calib_path)
