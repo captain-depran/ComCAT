ab7815e4254d9b6e9dacccf39a90dac0e701e3d9
Luke <lukescottnova@gmail.com>
2025-11-18 17:32:43 +0000

    Changed some logging, and target name

--- comet_finder_test.py
+++ comet_finder_test.py
@@ -23,24 +23,30 @@ comet_pics=[]
 
 
 all_image_names=photo_core.get_image_files(calib_path,tgt_name,filter)
-print(len(all_image_names))
+print("Images of Comet "+tgt_name+": ",len(all_image_names))
+if len(all_image_names)==0:
+    exit()
 #image_name=all_image_names[1]
 for image_name in all_image_names:
     img=photo_core.ESO_image(calib_path,image_name)
     if img.solved==False:
         continue
     comet_pic=photo_core.comet_frame(obs_code,search_name,img)
-    comet_pics.append(comet_pic)
-    comet_pic.find_comet()
+    comet_pic.find_comet(eph_code=0)
     #comet_pic.show_comet()
     comet_pic.cutout_comet()
+    comet_pics.append(comet_pic)
     all_frames.append(comet_pic.cutout)
 
 all_frames=np.array(all_frames)
+print(len(all_frames))
+if len(all_frames)==0:
+    print("ERROR: No Solved Images")
+    exit()
 
 cutout_stack = photo_core.composite_comet(all_frames)
 comet_error = photo_core.lock_comet(cutout_stack)
-photo_core.mark_target(comet_error+((len(cutout_stack[0])-1)/2),cutout_stack)
+#photo_core.mark_target(comet_error+((len(cutout_stack[0])-1)/2),cutout_stack)
 
 all_frames=[]
 for pic in comet_pics:
@@ -50,13 +56,10 @@ for pic in comet_pics:
 
     pic.refine_lock() #Refine the lock on the comet by refitting a gaussian per image now we know we are *basically* on top of the comet
 
-    
     all_frames.append(pic.cutout)
+    #pic.show_comet()
     #photo_core.mark_target([pic.pad,pic.pad],pic.cutout)
-
-
-
-all_frames=np.array(all_frames)
+pic.show_comet()
 
 cutout_stack = photo_core.composite_comet(all_frames)
 comet_error = photo_core.lock_comet(cutout_stack)
