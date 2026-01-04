da5c5e3da2dfdb9fc693f68e527fe5d3d14bcf34
captain-depran <lukescottnova@gmail.com>
2026-01-04 19:10:37 +0000

    Update target name to "P29" and change filter to "R#642"

--- ESO-DS1 Raw to plate solved.py
+++ ESO-DS1 Raw to plate solved.py
@@ -8,8 +8,8 @@ import ccdproc as ccdp
 root_dir = pathlib.Path(__file__).resolve().parent
 
 px_scale=0.24
-tgt_name="149P"
-filter="i#705"
+tgt_name="P29"
+filter="R#642"
 
 trim_tags=["HIERARCH ESO DET OUT1 PRSCX","HIERARCH ESO DET OUT1 PRSCY","HIERARCH ESO DET OUT1 OVSCX","HIERARCH ESO DET OUT1 OVSCY"]
 ref_image=pathlib.Path(root_dir/"Data_set_1"/"block_1"/"BIAS"/"FREE"/"EFOSC.2009-01-27T21_00_47.752.fits")
